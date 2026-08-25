1f566f786b2a0b2c7bf5e8ae8bde7fbdc935a191
john-gregtech <johngregtech@users.noreply.github.com>
2026-08-25 17:06:13 +1000

    just horsing around

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 //create a compile config for multiple main functions for TEST, CLIENT and SERVER
 #include <iostream>
 
+#if defined(WEE_TEST) 
+int main() {
+    std::cout << "those damn monkeys should be sent to the zoo!!\n";
+    return 0;
+}
+
+#else 
+
 int main() {
     std::cout << "chimp" << std::endl;
     return 0;
 }
+#endif
